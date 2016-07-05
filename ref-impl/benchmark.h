dc16c5fe0c2cb83f452373a25f479bb1aed06b97
Mikhail Maltsev <m.maltsev@corp.mail.ru>
2016-07-05 16:58:41 +0300

    Add reference implementation

---
+++ ref-impl/benchmark.h
@@ -0,0 +1,50 @@
+#ifndef BENCHMARK_H
+#define BENCHMARK_H
+
+#include <type_traits>
+
+namespace benchmark {
+
+#ifdef __x86_64__
+#define MAX_GPR_SIZE    8
+#else
+#define MAX_GPR_SIZE    4
+#endif
+
+template<typename T>
+__attribute__((always_inline))
+inline std::enable_if_t<sizeof(T) <= MAX_GPR_SIZE>
+keep(T&& x)
+{
+    asm volatile("" : : "r" (x) : );
+}
+
+template<typename T>
+__attribute__((always_inline))
+inline std::enable_if_t<(sizeof(T) > MAX_GPR_SIZE)>
+keep(T&& x)
+{
+    asm volatile("" : : "m" (x) : );
+}
+
+template<typename T>
+__attribute__((always_inline))
+inline std::enable_if_t<sizeof(T) <= MAX_GPR_SIZE>
+touch(T& x)
+{
+    static_assert(!std::is_const<T>(), "touch argument is writeable");
+    asm volatile("" : "+r" (x) : : );
+}
+
+template<typename T>
+__attribute__((always_inline))
+inline std::enable_if_t<(sizeof(T) > MAX_GPR_SIZE)>
+touch(T& x)
+{
+    static_assert(!std::is_const<T>(), "touch argument is writeable");
+    asm volatile("" : "+m" (x) : : );
+}
+
+}
+
+#endif
